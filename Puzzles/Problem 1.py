7a33195eb3778573599ad871869e2708fa0b19f4
Kyle <khawley1231@gmail.com>
2018-04-15 19:44:35 -0400

    Simple Problem 1 for testing git.

---
+++ Puzzles/Problem 1.py
@@ -0,0 +1,10 @@
+working_sum = 0
+test_num = 3
+end = 1000
+
+while test_num < 1000:
+    if test_num % 3 == 0 or test_num % 5 == 0:
+        working_sum += test_num
+    test_num += 3
+
+print(str(working_sum))
